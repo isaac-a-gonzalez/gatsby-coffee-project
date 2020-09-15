b1f0d47b4b01b181ca81e38c7ec3c8de3eaa5719
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2020-09-15 14:50:58 -0700

    menu section with buttons so filter menu items

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Roastio" />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -19,6 +21,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
