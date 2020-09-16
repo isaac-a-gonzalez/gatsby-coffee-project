5be10080c71b85aa870f8fae1abb121fb76c513f
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2020-09-15 19:13:36 -0700

    products and contact us component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,16 @@ import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
 import Menu from "../components/Home/Menu"
+import Products from "../components/Home/Products"
+import Contact from "../components/Home/Contact"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Roastio" />
     <Info />
     <Menu items={data.menu} />
+    <Products />
+    <Contact />
   </Layout>
 )
 
